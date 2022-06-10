3a2d942b8439ed75a0b6e083520335fd1159421f
alok <alok60541@gmail.com>
2022-06-10 21:50:34 +0530

    atm code

---
+++ Atm.java
@@ -0,0 +1,33 @@
+// Tution Question 3_September_2021
+
+// Q. Class bank account contains details about the account holder. It has methods deposite
+//  	and withdrawl along with its balance and display.
+//   Class Saving account inherits from bank account and additionly stores rate of interest(monthly)
+// 	It has method add interest that compute interest and adds it to balance.
+//    It overwrite display method of bank account .
+// 	formula for interest:  interest = balance * (rate/100) * (month/12);
+
+// ************************** Programming **********************
+
+import java.util.Scanner;
+
+// *** all details of customer ****
+class BankAccount{
+
+
+}
+
+// **** rate of interest *****
+class SavingAccount extends BankAccount{
+
+
+}
+
+public class Atm{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+	
+	}
+
+}
+
